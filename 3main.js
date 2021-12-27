9669e7c3bbffa5f7360cdab95d667e17512759f6
Neromedel <79370397+Neromedel@users.noreply.github.com>
2021-12-27 17:50:12 +0530

    Add files via upload

---
+++ 3main.js
@@ -0,0 +1,44 @@
+object = [];
+status = "";
+img = "";
+
+function preload() {
+    img = loadImage("plant.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.center();
+    object_detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById('Status').innerHTML = "Status : Detecting Objects."
+}
+
+function modelLoaded() {
+    console.log('Model Loaded.');
+    status = true;
+    object_detector.detect(img,gotResults);
+}
+
+function draw() {
+    image(img, 0, 0, 640, 480);
+    if(status != ""){
+    for(i = 0; i < object.length;i++){
+        document.getElementById("Status").innerHTML = "Status : Object Detected";
+        fill("Red");
+        percent = floor(object[i].confidence * 100);
+        text(object[i].label+" "+percent+"%",object[i].x+15,object[i].y+15);
+        noFill();
+        stroke("Red");
+        rect(object[i].x,object[i].y,object[i].width,object[i].height);
+    }
+    }
+}
+
+function gotResults(error,results){
+if(error){
+console.error(error);
+}
+console.log(results);
+object = results;
+}	
+
---
+++ main.js
@@ -0,0 +1,53 @@
+
+status = "";
+img = "";
+
+function preload() {
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    object_detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById('Status').innerHTML = "Status : Detecting Objects.";
+}
+
+function modelLoaded() {
+    console.log('Model Loaded.');
+    status = true;
+    object_detector.detect(img,gotResults);
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("lightseagreen");
+    text("Dog",140,40);
+    noFill();
+    stroke('lightseagreen');
+    rect(30,60,450,350);
+
+    fill("lightsalmon");
+    text("Cat",500,40);
+    noFill();
+    stroke('lightsalmon');
+    rect(300,90,270,320);
+}
+
+function gotResults(error,results){
+if(error){
+console.error(error);
+}
+console.log(results);
+}
+function one(){
+    window.location = "1.html"
+}
+
+function two(){
+    window.location = "2.html"
+}
+
+function three(){
+    window.location = "3.html"
+}
